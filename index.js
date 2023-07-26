9db6b6b320f88fdf21a92c53e7c9952870003ad4
Manuel P <m.perezasensio@gmail.com>
2023-07-25 17:19:56 -0700

    server working

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const cors = require('cors');
+const axios = require('axios');
+
+// if (process.env.NODE_ENV === 'development') {
+  const dotenv = require('dotenv');
+  dotenv.config();
+// }
+
+const app = express();
+app.use(cors());
+console.log(process.env.NODE_ENV)
+// to bypass CORS
+app.get('/', async (req, res) => {
+  
+  if (req.query.url) {
+    const proxyRes = await axios.get(`${req.query.url}`,{headers: {
+            "x-rapidapi-host": "api-football-v1.p.rapidapi.com",
+            "x-rapidapi-key": process.env.API_KEY,
+        }});
+    // const proxyRes = await axios.get(req.query.url);
+    res.send(proxyRes.data);
+  } else {
+    res.send('Please provide a url query parameter');
+  }
+});
+
+app.listen(5001, () => {
+  console.log('Listening on port 5001...');
+});
