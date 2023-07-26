dd619fbf5c1a1f16eec0a13b0407ee2fae0e9dc9
Manuel P <m.perezasensio@gmail.com>
2023-07-26 10:50:09 -0700

    axios updated

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(cors());
 console.log(process.env.NODE_ENV)
 // to bypass CORS
-app.get('/', async (req, res) => {
+app.get('/comparison', async (req, res) => {
   
   if (req.query.url) {
     const proxyRes = await axios.get(`${req.query.url}`,{headers: {
@@ -26,6 +26,42 @@ app.get('/', async (req, res) => {
   }
 });
 
+
+
+app.get('/', async (req, res) => {
+
+  if (req.query.url) {
+    if(req.query.season){
+      const proxyRes = await axios.get(`${req.query.url}`, {
+        headers: {
+          //"x-rapidapi-host": "https://api-football-v1.p.rapidapi.com/v3/",// this one for the rapid api server
+          "x-rapidapi-host": "v3.football.api-sports.io",// this one for the api-football server
+          "x-rapidapi-key": process.env.API_KEY,
+        },
+        params:{
+          team: req.query.team,
+          season: req.query.season,
+          league: req.query.league
+        }
+      });
+      // const proxyRes = await axios.get(req.query.url);
+      res.send(proxyRes.data);
+    } else {
+    const proxyRes = await axios.get(`${req.query.url}`, {
+      headers: {
+        //"x-rapidapi-host": "https://api-football-v1.p.rapidapi.com/v3/",// this one for the rapid api server
+        "x-rapidapi-host": "v3.football.api-sports.io",// this one for the api-football server
+        "x-rapidapi-key": process.env.API_KEY,
+      }
+    });
+    // const proxyRes = await axios.get(req.query.url);
+    res.send(proxyRes.data);}
+  } else {
+    res.send('Please provide a url query parameter');
+  }
+});
 app.listen(5001, () => {
   console.log('Listening on port 5001...');
 });
+
+
