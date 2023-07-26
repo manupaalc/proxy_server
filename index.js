adea24b01af882ab643907b544413f8a1c337e29
Manuel P <m.perezasensio@gmail.com>
2023-07-26 08:57:15 -0700

    back to api-football server

--- index.js
+++ index.js
@@ -15,9 +15,10 @@ app.get('/', async (req, res) => {
   
   if (req.query.url) {
     const proxyRes = await axios.get(`${req.query.url}`,{headers: {
-        "x-rapidapi-host": "https://api-football-v1.p.rapidapi.com/v3/teams/statistics",
-        "x-rapidapi-key": process.env.API_KEY,
-    }});
+            //"x-rapidapi-host": "https://api-football-v1.p.rapidapi.com/v3/",// this one for the rapid api server
+            "x-rapidapi-host": "v3.football.api-sports.io" // this one for the api-football server
+            "x-rapidapi-key": process.env.API_KEY,
+        }});
     // const proxyRes = await axios.get(req.query.url);
     res.send(proxyRes.data);
   } else {
